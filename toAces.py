aa8e4796fd3ec220d1ef5a052319f3636ab9d174
Enrique Velasco <enrique.velasco@students.salle.url.edu>
2019-05-24 23:03:50 +0200

    color space to aces 1.0.3

--- toAces.py
+++ toAces.py
@@ -13,6 +13,6 @@ for node in mc.ls(type=node_type):
     origin_color_type = mc.getAttr("{}.colorSpace".format(node)).lower()
     if origin_color_type in mapping_types.keys():
         mc.setAttr("{}.colorSpace".format(node), mapping_types[origin_color_type], type="string")
-        print('"{}" file has changed the color space from {} to {}'.format(node,
-                                                                           origin_color_type,
-                                                                           mapping_types[origin_color_type]))
+        print('"{}" file has changed the color space from "{}" to "{}"'.format(node,
+                                                                               origin_color_type,
+                                                                               mapping_types[origin_color_type]))
