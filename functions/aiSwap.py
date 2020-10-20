0910670dee4d0da693543788013497e9143680b1
Enrique Velasco <enrique.velasco@students.salle.url.edu>
2020-10-20 22:45:22 +0200

    ai swap

--- functions/aiSwap.py
+++ functions/aiSwap.py
@@ -1,8 +1,9 @@
 import maya.cmds as mc
+
+
 def ai_standard_material_to_lambert():
     for m in mc.ls(type="aiStandardSurface"):
         out_color_con = mc.listConnections(m + ".outColor", plugs=True)
-        print out_color_con
         col = mc.getAttr(m + ".baseColor")[0]
         lam = mc.createNode("lambert")
         mc.setAttr(lam + ".color", *col)
@@ -10,5 +11,6 @@ def ai_standard_material_to_lambert():
             mc.connectAttr(lam + ".outColor", con, f=True)
         mc.delete(m)
         mc.rename(lam, m)
+
 
 ai_standard_material_to_lambert()
