bda8c440d9bcc83ef82e6e1711d99341b2947bf5
Enrique Velasco <enrique.velasco@students.salle.url.edu>
2023-01-17 08:54:30 +0100

    added more examples

--- nodes/deformer_boilerplate1.py
+++ nodes/deformer_boilerplate1.py
@@ -19,6 +19,17 @@ class MyDefomer(ommpx.MPxDeformerNode):
     def deform(self, data_block, geo_iter, matrix, multi_index):
         """
         Compute when evaluating the node, we can modify the specific components of the geometry.
+        This method is executed after the compute method.
+        If there is a need to retrieve information such as the normals and in order not to have any cycles of evaluation
+        for extra computation, you can access to the input mesh data by calling:
+            input_handle = data_block.outputArrayValue(self.input)
+            input_handle.jumpToElement(multi_index)
+            input_element_handle = input_handle.outputValue()
+            input_geom = input_element_handle.child(self.inputGeom).asMesh()
+            mesh_fn = om.MFnMesh(input_geom)
+            normals = omFloatVectorArray()
+            mesh_fn.getVertexNormals(False, normals)
+
         :param data_block: (om.MDataBlock): Access to the deformer attributes
         :param geo_iter: (om.MItGeometry):  in order to move for the cvs or vertices of the geometry
         :param matrix: (om.MMatrix): as the components in the geo_iter are in local space, this matrix allows adding the object
@@ -31,12 +42,42 @@ class MyDefomer(ommpx.MPxDeformerNode):
         if envelope == 0:
             return
 
-        # example to iterate the geometry
+        # example to iterate the geometry, always reset the iterator to start by the first element
         geo_iter.reset()
         while not geo_iter.isDone():
             # get the painted weight of the component of the geometry
             source_weight = self.weightValue(data_block, multi_index, geo_iter.index())
             geo_iter.next()
+
+    def accessoryAttribute(self):
+        """
+        Returns the attribute where the accessory nodes will be connected
+        :return:
+        """
+        """
+        # EXAMPLE:
+        return MyDefomer.target_pos
+        """
+        pass
+
+    def accessoryNodeSetup(self, dag_modifier):
+        """
+        Operations that create the accessory network. There is no no need to implement undo and redo locgic
+        :param: dag_modifier: om.MDagModifier
+        :return:
+        """
+        """
+        # EXAMPLE: 
+        
+        locator = dag_modifier.create("locator")
+        
+        locator_fn = om.MFnDependencyNode(locator)
+        locator_translate_plug = locator_fn.findPlug("translate", False)
+        target_position_plug = om.MPlug(self.thisMObject(), MyDefomer.target_pos)
+        dag_modifier.connect(locator_translate_plug, target_position_plug)
+        """
+
+        pass
 
     @classmethod
     def creator(cls):
@@ -53,6 +94,7 @@ class MyDefomer(ommpx.MPxDeformerNode):
         cls.target_mesh = typed_attr.create("targetMesh", "tMesh", om.MFnData.kMesh)
 
         numeric_attr = om.MFnNumericAttribute()
+        # MFnNumericAttribute can have .create() .createPoint() .createColor()
         cls.target_weight = numeric_attr.create("targetWeight", "tWeight", om.MFnNumericData.kFloat, 0.0)
         numeric_attr.setKeyable(True)
         numeric_attr.setMin(0.0)
